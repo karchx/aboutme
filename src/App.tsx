00e080a8a6b13ed4d0c7295cec5502f5181c8857
Стив <kenethra.74@gmail.com>
2023-03-09 23:39:24 -0600

    feat: card links

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import {useState} from "react";
 import Typewriter from "typewriter-effect";
 import './App.css'
+import CardLinks from "./components/CardLinks";
 
 function App() {
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
     <>
       <div className="App">
@@ -13,10 +17,15 @@ function App() {
               .pauseFor(1000)
               .deleteAll()
               .typeString("kabout --help")
+              .callFunction(() => setShowInfo(true))
               .start();
           }}
         />
       </div>
+
+      {
+        showInfo ? <CardLinks /> : null
+      }
     </>
   )
 }
---
+++ src/components/CardLinks.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CardLinks = () => {
+  return (
+    <>
+      <div>
+        <a>hello...</a>
+      </div>
+    </>
+  )
+}
+
+export default CardLinks;
